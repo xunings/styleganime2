ab8a9eebf5d01c1d5c0dd4960e9650064b1af5ea
shaoxuning <shaoxuning@corp.netease.com>
2020-03-17 10:02:35 +0800

    Add ranker scripts

--- bash/run_ranker.sh
+++ bash/run_ranker.sh
@@ -4,13 +4,20 @@ usecuda100
 source /storage04/users/shaoxuning/.env/venv_py3.6_tf1.14gpu/bin/activate
 export CUDA_VISIBLE_DEVICES=1
 export PYTHONPATH=/storage04/users/shaoxuning/projects/acgface/try/stylegan2
-export SRC_DIR=/storage04/users/shaoxuning/projects/acgface/try/stylegan2/results/generate_images/kagura_psi0d8_1001to1100_20200227
+# export SRC_DIR=/storage04/users/shaoxuning/projects/acgface/try/stylegan2/results/generate_images/kagura_psi0d8_1001to1100_20200227
+export SRC_DIR=/storage04/users/shaoxuning/projects/acgface/try/stylegan2/results/generate_images/kagura_psi0d8_20200227
 export SCRIPT=/storage04/users/shaoxuning/projects/acgface/try/stylegan2/misc/ranker.py
 export MODEL=/storage04/users/shaoxuning/projects/acgface/try/stylegan2/models/2020-01-11-skylion-stylegan2-animeportraits-networksnapshot-024664.pkl
+export OUTFILE=rank/rank.txt
 
-find $SRC_DIR -type f -name "*.png" | xargs python3 $SCRIPT --model $MODEL --images | tee rank.txt
+find $SRC_DIR -type f -name "*.png" | xargs python3 $SCRIPT --model $MODEL --output $OUTFILE --images
 
-cat rank.txt | sort --field-separator ' ' --key 2 --numeric-sort | head -25 | cut -d ' ' -f1 | xargs -n1 -I{} cp {} rank/worst/
+mkdir rank/worst
+mkdir rank/best
 
-cat rank.txt | sort --field-separator ' ' --key 2 --numeric-sort | head -25 | cut -d ' ' -f1 | xargs -n1 -I{} cp {} rank/worst/
+echo 'Finding the worst...'
+cat $OUTFILE | sort --field-separator ' ' --key 2 --numeric-sort | head -25 | cut -d ' ' -f1 | xargs -n1 -I{} cp {} rank/worst/
+
+echo 'Finding the best...'
+cat $OUTFILE | sort --field-separator ' ' --key 2 --numeric-sort | tail -25 | cut -d ' ' -f1 | xargs -n1 -I{} cp {} rank/best/
 
--- misc/ranker.py
+++ misc/ranker.py
@@ -8,6 +8,7 @@ import cv2
 import dnnlib.tflib as tflib
 # import sys
 import argparse
+import PIL.Image
 
 
 def main(args):
@@ -15,15 +16,22 @@ def main(args):
     _G, D, _Gs = pickle.load(open(args.model, "rb"))
 
     for file_path in args.images:
-        img = cv2.imread(file_path, cv2.IMREAD_COLOR)
+        # print(file_path)
+        img = np.asarray(PIL.Image.open(file_path))
+        img = img.reshape(1, 3, 512, 512)
+
+        # img = cv2.imread(file_path, cv2.IMREAD_COLOR)
+
         # See dataset_tool.display, the image input must be RGB and NCHW
         # But the img read by cv2 is BGR and NHWC
-        img = img.transpose((2, 0, 1))  # HWC -> CHW
-        img = img[::-1, :, :]  # BGR -> RGB
-        img = np.expand_dims(img, axis=0)
+        # img = img.transpose((2, 0, 1))  # HWC -> CHW
+        # img = img[::-1, :, :]  # BGR -> RGB
+        # img = np.expand_dims(img, axis=0)
         # img = img.reshape(1, 3, 512, 512)
         score = D.run(img, None)
         print(file_path, score[0][0])
+        with open(args.output, 'a') as fout:
+            fout.write(file_path + ' ' + str(score[0][0]) + '\n')
 
 
 def parse_arguments():
@@ -31,6 +39,7 @@ def parse_arguments():
     parser.add_argument('--model', type=str, required=True,
                         help='.pkl model')
     parser.add_argument('--images', nargs='+')
+    parser.add_argument('--output', type=str, default='rank.txt')
     return parser.parse_args()
 
 
