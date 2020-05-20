518299d9c34a835180bca1c3def1de272fdb4707
shaoxuning <shaoxuning@corp.netease.com>
2020-05-20 11:08:17 +0800

    Update kagura path

--- bash/run_ranker.sh
+++ bash/run_ranker.sh
@@ -4,8 +4,8 @@ usecuda100
 source /storage04/users/shaoxuning/.env/venv_py3.6_tf1.14gpu/bin/activate
 export CUDA_VISIBLE_DEVICES=1
 export PYTHONPATH=/storage04/users/shaoxuning/projects/acgface/try/stylegan2
-# export SRC_DIR=/storage04/users/shaoxuning/projects/acgface/try/stylegan2/results/generate_images/kagura_psi0d8_1001to1100_20200227
-export SRC_DIR=/storage04/users/shaoxuning/projects/acgface/try/stylegan2/results/generate_images/kagura_psi0d8_20200227
+# export SRC_DIR=/storage04/users/shaoxuning/projects/acgface/try/stylegan2/results/generate_images/kagura-010060-20200227/kagura_psi0d8_1001to1100_20200227
+export SRC_DIR=/storage04/users/shaoxuning/projects/acgface/try/stylegan2/results/generate_images/kagura-010060-20200227/kagura_psi0d8_20200227
 export SCRIPT=/storage04/users/shaoxuning/projects/acgface/try/stylegan2/misc/ranker.py
 export MODEL=/storage04/users/shaoxuning/projects/acgface/try/stylegan2/models/2020-01-11-skylion-stylegan2-animeportraits-networksnapshot-024664.pkl
 # export MODEL=/storage04/users/shaoxuning/projects/acgface/try/stylegan2/models/kagura-010060-20200227.pkl
